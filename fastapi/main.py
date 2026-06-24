a30c3179a09ac00cbfe31ebe9164bd75c33f3166
Sivasankar Thalavai <s2.thalavai@gmail.com>
2026-06-24 17:04:16 +0530

    first fast api

--- fastapi/main.py
+++ fastapi/main.py
@@ -2,10 +2,10 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-def get_shipment():
+def get_shipment(shipment_id: int):
     # Placeholder function to retrieve shipment details
-    return {"shipment_id": 123, "status": "In Transit"} 
+    return {"shipment_id": shipment_id, "status": "In Transit"} 
 
 @app.get("/shipment/{shipment_id}")
 def read_shipment(shipment_id: int):
-    return get_shipment()       
+    return get_shipment(shipment_id)       
